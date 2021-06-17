fb90835da76ca4b0d98f84c57cbd5bc1f53ffe68
Adi Cosma <adi@stripe.com>
2021-06-17 12:52:40 +0100

    set radar session id

--- create-radar-session/create-session.js
+++ create-radar-session/create-session.js
@@ -1,6 +1,7 @@
 var stripe = Stripe('pk_test_AnOcpECn5sgSsIWl0lgN4n7d002LJtdPkg',
                 {canCreateRadarSession: true});
-
+var session;
+                
 stripe.createRadarSession().then(function(result) {
   if (result.error) {
     // Typically you should not abort your checkout flow if an error is returned.
@@ -9,6 +10,7 @@ stripe.createRadarSession().then(function(result) {
     // Send the Radar Session to your server or store it and
     // send later when the user submits their payment details.
     //radarSessionHandler(result.radarSession);
-    document.getElementById("session_id").outerText = result.radarSession;
+    session = result.radarSession;
+    document.getElementById("session_id").outerText = result.radarSession.id;
   }
 });
