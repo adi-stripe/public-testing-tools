37a0c2543405f9f67b257e777c109988ee2f7909
Adi Cosma <adi@stripe.com>
2021-06-18 13:50:29 +0100

    add radar session

--- create-radar-session/confirm-payment.js
+++ create-radar-session/confirm-payment.js
@@ -1,11 +1,26 @@
 var stripe = Stripe('pk_test_AnOcpECn5sgSsIWl0lgN4n7d002LJtdPkg');
+var radar_session;
+
+stripe.createRadarSession().then(function(result) {
+    if (result.error) {
+      // Typically you should not abort your checkout flow if an error is returned.
+      console.error(result.error);
+    } else {
+      // Send the Radar Session to your server or store it and
+      // send later when the user submits their payment details.
+      radar_session = result.radarSession.id;
+    }
+  });
 
 function confirmPayment() {
     var pi_secret = document.getElementById("pi_secret").value;
     var pm_id = document.getElementById("pm_id").value;
      
     stripe.confirmCardPayment(pi_secret, {
-        payment_method: pm_id
+        payment_method: pm_id,
+        radar_options: {
+            session = radar_session
+        }
     }).then(function(result) {
         var result_message = document.getElementById("result_message");
         if (result.error) {
