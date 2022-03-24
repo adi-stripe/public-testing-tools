482fa106b64909800a2d678557b68890962f9a4c
Adi Cosma <adi@stripe.com>
2022-03-24 22:12:57 +0000

    add element

--- test-cvc-setup-intents/checkout.js
+++ test-cvc-setup-intents/checkout.js
@@ -2,7 +2,7 @@ var stripe = Stripe('pk_test_AnOcpECn5sgSsIWl0lgN4n7d002LJtdPkg');
 var elements = stripe.elements();
 
 //mount cvc element
-var cvc = elements.create('cardCvc');
+var cvc = elements.create("cardCvc");
 cvc.mount("#cvc-element");
 
 //confirm setup intent
